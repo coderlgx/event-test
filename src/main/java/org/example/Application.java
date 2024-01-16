614852faa7ca09a674ff17e1b39d882e9b53b408
coderLgx <2190685582@qq.com>
2024-01-16 23:13:09 +0800

    init

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Application {
+    public static void main(String[] args) {
+        String path = System.getProperty("FC_CUSTOM_CONTAINER_EVENT");
+        System.out.println(path);
+        System.out.println("来过");
+    }
+}
